fd2d09e02d8078dfc57cc6d68e33cd26ccc13230
HuuNguyen <huukongu@gmail.com>
2026-09-12 14:04:51 +0700

    remove folder

--- merchant-service/src/main/java/com/ptpmhdv/merchantservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ptpmhdv.merchantservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
